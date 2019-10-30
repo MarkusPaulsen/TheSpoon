ca58930f020034ebf061511870d3ffe2a6375fa2
Andre <a.aing.pro@gmail.com>
2019-10-30 13:24:46 +0100

    Create the Log in menu form

--- thespoon/src/components/authentification/LogIn.js
+++ thespoon/src/components/authentification/LogIn.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 class LogIn extends Component {
-    render() {
-        return (
-            <label>Login page</label>
-        );
-      }
-    }
+  render() {
+    return (
+      <div className="container">
+        <h1> The Spoon </h1>
+        <form>
+          <h5>Log in</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="input-field">
+            <button>Login</button>
+          </div>
+          <div>
+            <Link to="/SignUp">Don't have an account yet? Register now</Link>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 export default LogIn;
-    
+
