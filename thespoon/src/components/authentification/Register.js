6cfa739e15d79206f43d54a709c7841c0a5f08e4
Lora Žuliček <lora.zulicek@fer.hr>
2019-11-03 23:11:50 +0100

    #19 changed register form depending on user role

--- thespoon/src/components/authentification/Register.js
+++ thespoon/src/components/authentification/Register.js
@@ -12,31 +12,42 @@ class Register extends Component  {
           <div className="account-type">
             <h4>as a <span className="role">{this.props.role}</span></h4>
           </div>
+
           <div className="input-field name">
             <IconName />
             <input type="text" id="firstname" name="firstname" placeholder="First name" required/>
           <input type="text" id="surname" name="surname" placeholder="Surname" required/>
           </div>
-          <div className="input-field">
-            <IconLocation />
-          <input type="text" id="nationality" name="nationality" placeholder="Nationality" required/>
-          </div>
-          <div className="input-field">
-            <IconBirthday />
-          <input type="date" id="birth-date" name="birth-date" required/>
-          </div>
+
+         {this.props.role == "customer" ? 
+            <div className="input-field">
+               <IconLocation />
+              <input type="text" id="nationality" name="nationality" placeholder="Nationality" required/>
+            </div>
+          : null }
+
+          {this.props.role == "customer" ? 
+            <div className="input-field">
+              <IconBirthday />
+              <input type="date" id="birth-date" name="birth-date" required/>
+            </div>
+          : null }
+
           <div className="input-field">
             <IconEmail />
-          <input type="email" id="email" name="email" placeholder="E-mail" required/>
+            <input type="email" id="email" name="email" placeholder="E-mail" required/>
           </div>
-          <div className="input-field">
-           <IconPassword />
-          <input type="password" name="password" id="password" placeholder="Password" required/>
-          </div> 
+
           <div className="input-field">
             <IconPassword />
-          <input type="confirm-password" id="confirm-password" name="confirm-password" placeholder="Confirm password" required/>
+            <input type="password" name="password" id="password" placeholder="Password" required/>
+          </div> 
+
+          <div className="input-field">
+            <IconPassword />
+            <input type="confirm-password" id="confirm-password" name="confirm-password" placeholder="Confirm password" required/>
           </div>
+          
           <button type="submit" className="normal">Sign up</button>
         </form>
     );
