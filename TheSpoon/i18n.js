dfd29a0021663d25875a77c62bb3e5518503dd96
Cathrine Akre-Aas <cathrine-97@hotmail.com>
2019-11-05 22:05:34 +0100

    Add i18n for localization

--- TheSpoon/i18n.js
+++ TheSpoon/i18n.js
@@ -1,28 +1,24 @@
-import { Localization } from 'expo';
-import i18n from 'i18next';
-import { reactI18nextModule } from 'react-i18next';
+import { Localization } from "expo";
+import i18n from "i18next";
+import { reactI18nextModule } from "react-i18next";
 
 const resources = {
-  fr: {
-    foo: 'como telle fous',
-    bar: 'chatouiller {{someValue}}',
+  no: {
+    password: "Passord"
   },
   en: {
-    foo: 'Foo',
-    bar: 'Bar {{someValue}}',
-  },
+    password: "Password"
+  }
 };
 
-i18n
-  .use(reactI18nextModule)
-  .init({
-    resources,
-    lng: Localization.locale,
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
-    },
-    cleanCode: true,
-  });
+i18n.use(reactI18nextModule).init({
+  resources,
+  lng: Localization.locale,
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false
+  },
+  cleanCode: true
+});
 
 export default i18n;
