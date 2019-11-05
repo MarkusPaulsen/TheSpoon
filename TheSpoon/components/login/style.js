c8ddde1c2051513e388e490b6b2f3a9452d1769f
Cathrine Akre-Aas <cathrine-97@hotmail.com>
2019-11-05 22:07:56 +0100

    Create simple login page for customer

---
+++ TheSpoon/components/login/style.js
@@ -0,0 +1,36 @@
+const React = require("react-native");
+
+const { Stylesheet } = React;
+
+export default {
+  container: {
+    flex: 1
+  },
+  text2: {
+    color: "rgba(213,85,141,1)",
+    fontSize: 24,
+    marginTop: 204,
+    alignSelf: "center"
+  },
+  textInput: {
+    width: 224,
+    height: 42,
+    color: "#121212",
+    marginTop: 99,
+    alignSelf: "center"
+  },
+  textInput2: {
+    width: 224,
+    height: 44,
+    color: "#121212",
+    marginTop: 14,
+    alignSelf: "center"
+  },
+  loginButton: {
+    width: 100,
+    height: 36,
+    backgroundColor: "rgba(46,236,187,1)",
+    marginTop: 6,
+    alignSelf: "center"
+  }
+};
