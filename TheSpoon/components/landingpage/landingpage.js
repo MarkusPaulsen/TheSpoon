67b1cdecd18c75fcdf27784a7c7b66171c59b916
janinestang <helenejs@stud.ntnu.no>
2019-11-06 16:14:22 +0100

    Fixed design for landingpage.js

--- TheSpoon/components/landingpage/landingpage.js
+++ TheSpoon/components/landingpage/landingpage.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import styles from "./landingpagestyle";
 import {
-  StyleSheet,
   View,
   StatusBar,
   Text,
-  TextInput,
-  Button,
   Alert, 
   Linking, 
   TouchableOpacity, 
-  Image
+  Image, 
+  ImageBackground
 } from "react-native";
 
 
@@ -18,29 +16,34 @@ export default class LandingPage extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar animated={false} hidden={false} />
-        <Image source={require('/Users/janinestang/git/TheSpoon/TheSpoon/assets/thespoon_logo_black.png')} style={{alignSelf: 'center'}}/>
-        <View style = {styles.buttons}> 
-            <TouchableOpacity
-                style = {styles.loginButton}
-                onPress={() => Alert.alert("Login Button pressed")}
-                onPress={() => this.onLoginPress()}
-                onPress={() => this.props.navigation.navigate('Login')} >
-                <Text style = {styles.smallText}>
-                    Log in
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity 
-                style = {styles.signupButton}
-                onPress={() => Alert.alert("Sign Up Button pressed")}
-                onPress={() => this.onLoginPress()}
-                onPress={() => Linking.openURL('https://google.com')} >
-                <Text style = {styles.smallText}>
-                    Sign up
-                </Text>
-            </TouchableOpacity>
-         </View>
+      <View>
+        <ImageBackground source={require('/Users/janinestang/git/TheSpoon/TheSpoon/assets/backgroundspoon.png')} style={{width: '100%', height: '100%'}}>
+            <View style={styles.container}>
+                <Image source={require('/Users/janinestang/git/TheSpoon/TheSpoon/assets/thespoon_logo_black.png')} style={styles.logo}/>
+                <View style = {styles.buttons}> 
+                    <TouchableOpacity
+                        style = {styles.loginButton}
+                        activeOpacity={.9}
+                        onPress={() => Alert.alert("Login Button pressed")}
+                        onPress={() => this.onLoginPress()}
+                        onPress={() => this.props.navigation.navigate('Login')} >
+                        <Text style = {styles.smallText}>
+                            Log in
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        style = {styles.signupButton}
+                        activeOpacity={.9}
+                        onPress={() => Alert.alert("Sign Up Button pressed")}
+                        onPress={() => this.onLoginPress()}
+                        onPress={() => Linking.openURL('https://google.com')} >
+                        <Text style = {styles.smallText}>
+                            Sign up
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+         </ImageBackground>
       </View>
     );
   }
--- TheSpoon/components/landingpage/landingpagestyle.js
+++ TheSpoon/components/landingpage/landingpagestyle.js
@@ -1,20 +1,14 @@
+
 const React = require("react-native");
+
 const { Stylesheet } = React;
+
 export default {
   container: {
-    alignSelf: 'center',
-    height: 100
-  },
-  bigText: {
-    color: "rgba(213,85,141,1)",
-    fontSize: 24,
-    marginTop: 204,
-    alignSelf: "center"
-  },
-  smallText: {
-    color: "#000000",
-    alignSelf: "center", 
-    marginTop: 9
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-around'
   },
   loginButton: {
     width: 203,
@@ -22,7 +16,7 @@ export default {
     backgroundColor: "#F3A3A3",
     marginTop: 6,
     alignSelf: "center",
-    borderRadius: 20
+    borderRadius: 20, 
   }, 
   signupButton: {
     width: 203,
@@ -30,9 +24,15 @@ export default {
     backgroundColor: "#A5DED0",
     marginTop: 6,
     alignSelf: "center",
-    borderRadius: 20
+    borderRadius: 20, 
+  }, 
+  smallText: {
+    color: "#000000",
+    alignSelf: "center", 
+    marginTop: 9
   }, 
   buttons: {
-
+    flexDirection: 'column',
+    justifyContent: 'space-around'
   }
 };
