c07f2ab6f5747378aa3a745eadaa11a02b9a927e
Cathrine Akre-Aas <cathrine-97@hotmail.com>
2019-11-06 15:18:32 +0100

    Add styling on email & password field

--- TheSpoon/components/login/login.js
+++ TheSpoon/components/login/login.js
@@ -3,36 +3,56 @@ import styles from "./loginstyle";
 import {
   StyleSheet,
   View,
-  StatusBar,
   Text,
   TextInput,
   Button,
-  Alert, 
-  Linking, 
+  Alert,
+  Linking,
   TouchableOpacity
 } from "react-native";
 
 export default class LoginScreen extends Component {
-
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar animated={false} hidden={false} />
         <Text style={styles.text2}>Log in</Text>
-        <TextInput placeholder="Username" style={styles.textInput} />
-        <TextInput placeholder="Password" style={styles.textInput} />
-        <Button 
-            title="Login"
-            style={styles.loginButton}
-            onPress={() => Alert.alert("Login Button pressed")}
-            onPress={() => this.onLoginPress()}
-            onPress={() => this.props.navigation.navigate('Home')}
+        <TextInput
+          placeholder="Username"
+          placeholderTextColor="#959595"
+          multiline
+          numberOfLines={1}
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.textInput}
         />
-         <TouchableOpacity>
-            <Text style = {styles.loginButton}>
-              
-            </Text>
-         </TouchableOpacity>
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor="#959595"
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.textInput}
+          returnKeyType="go"
+          secureTextEntry={true}
+        />
+        <Button
+          type="solid"
+          title="Login"
+          color="#F3A3A3"
+          onPress={() => this.onLoginPress()}
+          onPress={() => this.props.navigation.navigate("Home")}
+          style={styles.loginButton}
+        />
+        <Button
+          color="#A5DED0"
+          type="solid"
+          title="Not registered yet?"
+          onPress={() => this.onLoginPress()}
+          onPress={() => Linking.openURL("https://google.com")}
+          style={styles.loginButton}
+        />
+        <TouchableOpacity>
+          <Text style={styles.loginButton}></Text>
+        </TouchableOpacity>
       </View>
     );
   }
