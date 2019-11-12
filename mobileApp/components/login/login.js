6fd0f6fe3a51b6d2cc16f2ae5772ffa7f0a142d8
janinestang <helenejs@stud.ntnu.no>
2019-11-12 11:39:06 +0100

    Implemented login validation

--- mobileApp/components/login/login.js
+++ mobileApp/components/login/login.js
@@ -8,96 +8,126 @@ import {
   Image,
   StyleSheet
 } from "react-native";
-import UsernameIcon from '../../assets/login-email.png';
-import PasswordIcon from '../../assets/login-password.png';
-import CustomizedButton from '../button.js';
-
-
+import UsernameIcon from "../../assets/login-email.png";
+import PasswordIcon from "../../assets/login-password.png";
+import Validate from "./validation.js";
+import validate from "./validation";
 
 export default class LoginScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       username: "",
+      usernameError: "",
       password: "",
+      passwordError: "",
       token: ""
     };
+    this.register = this.register.bind(this);
   }
   async handleLogin() {
-      //TODO: Add token
-      // TODO: test navigation
-      try {
-          let data = JSON.stringify({email: this.state.username,
-              password: this.state.password,
-              isRestaurantOwner: false})
-          ;
-          let res = await fetch('http://192.168.1.110:5000/api/user/login/', {
-              method: 'POST',
-              headers: {
-                  Accept: 'application/json',
-                  'Content-Type': 'application/json',
-              },
-              body: data,
-          });
-          res = await res.text();
-          console.log(res);
-          //have not tested this yet, do not have any valid username to test
-          //if(res.ok){
-            //  this.props.navigation.navigate('Home');
-         // }
-      } catch (e) {
-          console.error(e);
-      }
+    //TODO: Add token
+    // TODO: test navigation
+    try {
+      let data = JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+        isRestaurantOwner: false
+      });
+      let res = await fetch("http://192.168.1.112:5000/api/user/login/", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: data
+      });
+      res = await res.text();
+      console.log(res);
+      //have not tested this yet, do not have any valid username to test
+      //if(res.ok){
+      //  this.props.navigation.navigate('Home');
+      // }
+    } catch (e) {
+      console.error(e);
+    }
   }
 
+  register() {
+    const usernameError = Validate("username", this.state.username);
+    const passwordError = Validate("password", this.state.password);
 
-  handleUsernameChange = (username) => {
-    this.setState({ username: username });
+    this.setState({
+      usernameError: usernameError,
+      passwordError: passwordError
+    });
+    if (!usernameError && !passwordError) {
+      this.props.navigation.navigate("Home");
+    }
+  }
+
+  handleUsernameChange = username => {
+    this.setState({ username: username.trim() });
   };
 
-  handlePasswordChange = (password) => {
-    this.setState({ password: password });
+  handlePasswordChange = password => {
+    this.setState({ password: password.trim() });
   };
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>Log in</Text>
-        <View style={{ flex: 2, justifyContent: 'space-around' }}>
+        <View style={{ flex: 2, justifyContent: "space-around" }}>
           <View style={{ flexDirection: "row" }}>
             <Image source={UsernameIcon} style={{ alignSelf: "center" }} />
             <TextInput
-              placeholder="Username"
               value={this.state.username}
               onChangeText={this.handleUsernameChange}
-              placeholderTextColor="#959595"
-              numberOfLines={1}
-              autoCapitalize="none"
-              returnKeyType="next"
-              keyboardType="email-address"
-              autoCorrect={false}
+              onBlur={() => {
+                this.setState({
+                  usernameError: validate("username", this.state.username)
+                });
+              }}
+              error={this.state.usernameError}
               style={styles.textInput}
             />
+            <Text style={{ color: "#F3A3A3" }}>
+              {this.state.usernameError ? "*" : null}
+            </Text>
           </View>
           <View style={{ flexDirection: "row", marginBottom: 90 }}>
             <Image source={PasswordIcon} style={{ alignSelf: "center" }} />
             <TextInput
-              placeholder="Password"
-              value={this.state.password}
               onChangeText={this.handlePasswordChange}
-              placeholderTextColor="#959595"
-              autoCapitalize="none"
-              autoCorrect={false}
-              style={styles.textInput}
-              returnKeyType="done"
+              value={this.state.password}
+              onBlur={() => {
+                this.setState({
+                  passwordError: Validate("password", this.state.password)
+                });
+              }}
+              error={this.state.passwordError}
               secureTextEntry={true}
+              style={styles.textInput}
             />
+            <Text style={{ color: "#F3A3A3" }}>
+              {this.state.passwordError ? "*" : null}
+            </Text>
           </View>
+          <Text style={{ color: "#F3A3A3" }}>
+            {this.state.usernameError || this.state.passwordError
+              ? "All fields must be filled out"
+              : null}
+          </Text>
         </View>
         <View style={{ flex: 1 }}>
-          <CustomizedButton label="Log in"
-          onPress={() => this.handleLogin()} style={styles.loginButton}
-          />
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={this.register}
+            style={styles.loginButton}
+          >
+            <Text style={styles.buttonText}> Log in </Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.registration}>
           <Text>Don't have an account?</Text>
@@ -120,9 +150,9 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 48,
-    fontFamily: 'roboto',
+    fontFamily: "roboto",
     marginTop: 150,
-    flex: 1,
+    flex: 1
     //justifyContent: 'center'
   },
   textInput: {
@@ -141,7 +171,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     backgroundColor: "#F3A3A3",
     marginTop: 6,
-    alignSelf: "center",
+    alignSelf: "center"
     //marginBottom: 50
   },
   registrationButton: {
@@ -155,9 +185,13 @@ const styles = StyleSheet.create({
     //textAlign: 'center',
     //justifyContent: 'space-between',
     flexDirection: "row",
-    flex: 1,
+    flex: 1
     //marginTop: 50
-
+  },
+  buttonText: {
+    color: "#000000",
+    alignSelf: "center",
+    marginTop: 9,
+    fontFamily: "roboto"
   }
-}
-);
+});
