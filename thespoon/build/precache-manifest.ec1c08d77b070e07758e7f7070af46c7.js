e1cf955073ad92d628dc7353b5880960a9609dbb
Markus Paulsen <ga96yen@mytum.de>
2019-11-13 15:19:13 +0100

    Removed entries from

---
+++ thespoon/build/precache-manifest.ec1c08d77b070e07758e7f7070af46c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057b44e610cdccf02128274d305879dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "561cb632dbd4c1f8b26f",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "f2cde47413eb63436a3a",
+    "url": "/static/css/main.f63c9b09.chunk.css"
+  },
+  {
+    "revision": "561cb632dbd4c1f8b26f",
+    "url": "/static/js/2.29d4dc6c.chunk.js"
+  },
+  {
+    "revision": "f2cde47413eb63436a3a",
+    "url": "/static/js/main.3a47a472.chunk.js"
+  },
+  {
+    "revision": "1c275158807852296cb7",
+    "url": "/static/js/runtime-main.621b31dd.js"
+  }
+]);
