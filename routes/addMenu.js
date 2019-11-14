6187a99f43f85a8a597e9c2b7a5fd588319c5fb8
xXEmilioXx <38227115+xXEmilioXx@users.noreply.github.com>
2019-11-14 18:35:17 +0100

    Add 2 middlewares

--- routes/addMenu.js
+++ routes/addMenu.js
@@ -2,21 +2,14 @@ const express = require('express');
 const router = express();
 router.use(express.json());
 
-const Owner = require('../models/owner.js');
+const Restaurant = require('../models/restaurant.js');
 
 const auth = require('../middleware/authorizationMiddleware.js');
+const isOwner = require('../middleware/checkIfOwnerMiddleware.js');
+const findRestaurant = require('../middleware/findRestaurantOfOwnerMiddleware.js');
 
-router.post('/', auth, async (req, res) => {
-    //check if the username is a restaurant owner's username
-    const owner = await Owner.findAll({
-        where: {
-            Username: req.body.username
-        }
-    });
-    if (owner.length <= 0) res.status(401).send('Access denied');
-    else {
+router.post('/', auth, isOwner, findRestaurant, async (req, res) => {
 
-    }
 });
 
 module.exports = router;
