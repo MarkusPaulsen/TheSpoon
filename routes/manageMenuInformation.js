ccdd1fe3cd59b7308762a2c0f7beb68af3063063
xXEmilioXx <38227115+xXEmilioXx@users.noreply.github.com>
2019-11-21 23:35:15 +0100

    Implement Add, Edit and Delete of a menu

--- routes/manageMenuInformation.js
+++ routes/manageMenuInformation.js
@@ -5,8 +5,6 @@ router.use(express.json());
 const Menu = require('../models/menu.js');
 const Tag = require('../models/tag.js');
 const TaggedMenu = require('../models/taggedMenu.js');
-const MenuItem = require('../models/menuItem.js');
-const TaggedItem = require('../models/taggedItem.js');
 
 const auth = require('../middleware/authorizationMiddleware.js');
 const isOwner = require('../middleware/checkIfOwnerMiddleware.js');
@@ -17,7 +15,7 @@ const menuItem = require('./menuItem.js');
 //Add an empty menu to a restaurant
 router.post('/', auth, isOwner, findRestaurant, async (req, res) => {
     //only tags already present in the database, in the table Tag, are allowed to used
-    //so first check if the tags of the menu and the tags of the menuItems are in the database
+    //so first check if the tags of the menu are in the database
 
     //check if the tags of the menu exist in the database
     for (let tag of req.body.tags) {
@@ -27,18 +25,6 @@ router.post('/', auth, isOwner, findRestaurant, async (req, res) => {
             }
         });
         if (tagFound.length <= 0) return res.status(400).send('One or more tags are not valid');
-    }
-
-    //check if the tags of every menuItem exist in the database
-    for (let menuItem of req.body.menuItems) {
-        for (let tag of menuItem.tags) {
-            const tagFound = await Tag.findAll({
-                where: {
-                    Name: tag
-                }
-            });
-            if (tagFound.length <= 0) return res.status(400).send('One or more tags are not valid');
-        }
     }
 
     //create the menu
@@ -56,25 +42,6 @@ router.post('/', auth, isOwner, findRestaurant, async (req, res) => {
         });
     }
 
-    //create the menuItems
-    for (let menuItem of req.body.menuItems) {
-        const menuItemCreated = await MenuItem.create({
-            Menu_ID: menuCreated.dataValues.Menu_ID,
-            Name: menuItem.name,
-            Description: menuItem.description,
-            Price: menuItem.priceEuros,
-            ImageLink: menuItem.imageLink
-        });
-
-        //for each menuItem created, associate it to the tags it has
-        for (let tag of menuItem.tags) {
-            await TaggedItem.create({
-                MI_ID: menuItemCreated.dataValues.MI_ID,
-                Tag: tag
-            });
-        }
-    }
-
     //send the response
     res.status(200).send({menuID: menuCreated.dataValues.Menu_ID});
 });
@@ -87,11 +54,76 @@ router.get('/', auth, isOwner, findRestaurant, async (req, res) => {
 //Edit a menu's information (not its items)
 router.put('/:menuID', auth, isOwner, findRestaurant, async (req,res) => {
 
+    //check if the menu with given menuID exist
+    const menuFound = await Menu.findAll({
+        where: {
+            Menu_ID: req.params.menuID
+        }
+    });
+    if (menuFound.length <= 0) return res.status(404).send('Menu not found');
+
+    //check if the tags of the menu exist in the database
+    for (let tag of req.body.tags) {
+        const tagFound = await Tag.findAll({
+            where: {
+                Name: tag
+            }
+        });
+        if (tagFound.length <= 0) return res.status(400).send('One or more tags are not valid');
+    }
+
+    //edit the corresponding row in Menu table
+    await Menu.update({
+        Restaurant_ID: req.restaurant.Restaurant_ID,
+        Name: req.body.name,
+        Description: req.body.description},
+        {
+            where: {
+                Menu_ID: req.params.menuID
+            }
+    });
+
+    //remove all the previous associated tags in the database
+    await TaggedMenu.destroy({
+        where: {
+            Menu_ID: req.params.menuID
+        }
+    });
+
+    //associate the menu to each tag it has
+    for (let tag of req.body.tags) {
+        await TaggedMenu.create({
+            Menu_ID: req.params.menuID,
+            Tag: tag
+        });
+    }
+
+    //send the response
+    res.status(200).send({menuID: req.params.menuID});
 });
 
 //Delete a menu
 router.delete('/:menuID', auth, isOwner, findRestaurant, async (req,res) => {
 
+    //check if the menu with given menuID exist
+    const menuFound = await Menu.findAll({
+        where: {
+            Menu_ID: req.params.menuID
+        }
+    });
+    if (menuFound.length <= 0) return res.status(404).send('Menu not found');
+
+    //there is no need to manually delete the associated tags in the database: deleting the menu will also delete them
+    //(on delete cascade)
+    //delete the menu
+    await Menu.destroy({
+        where: {
+            Menu_ID: req.params.menuID
+        }
+    });
+
+    //send the response
+    res.status(200).send({menuID: req.params.menuID});
 });
 
 //this is required in order to route the request to the endpoints that manage the menu items
