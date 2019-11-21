6a4a7eec6fc22b95928f5411f948c2958c7fd7a6
Ida Merete Enholm <ida.enholm@me.com>
2019-11-21 21:11:23 +0100

    Added colors and typography files

---
+++ mobileApp/styles/typography.js
@@ -0,0 +1,74 @@
+import * as Colors from "./colors";
+
+// FONT FAMILY
+const FONT_FAMILY = "roboto";
+
+// FONT WEIGHT
+const FONT_WEIGHT_LIGHT = "300";
+const FONT_WEIGHT_MEDIUM = "500";
+
+// FONT SIZE
+const FONT_SIZE_36 = 36;
+const FONT_SIZE_24 = 24;
+const FONT_SIZE_18 = 18;
+const FONT_SIZE_15 = 15;
+const FONT_SIZE_12 = 12;
+const FONT_SIZE_10 = 10;
+
+// FONT STYLE
+export const FONT_H2_PINK = {
+  fontFamily: FONT_FAMILY,
+  fontWeight: FONT_WEIGHT_MEDIUM,
+  fontSize: FONT_SIZE_36,
+  color: Colors.PINK
+};
+
+export const FONT_H4_BLACK = {
+  fontFamily: FONT_FAMILY,
+  fontWeight: FONT_WEIGHT_MEDIUM,
+  fontSize: FONT_SIZE_18,
+  color: Colors.BLACK
+};
+
+export const FONT_H4_PINK = {
+  fontFamily: FONT_FAMILY,
+  fontWeight: FONT_WEIGHT_MEDIUM,
+  fontSize: FONT_SIZE_18,
+  color: Colors.PINK
+};
+
+export const FONT_H4_GRAY_DARK = {
+  fontFamily: FONT_FAMILY,
+  fontWeight: FONT_WEIGHT_MEDIUM,
+  fontSize: FONT_SIZE_18,
+  color: Colors.GRAY_DARK
+};
+
+export const FONT_INPUT = {
+  fontFamily: FONT_FAMILY,
+  fontWeight: FONT_WEIGHT_MEDIUM,
+  fontSize: FONT_SIZE_15,
+  color: Colors.BLACK
+};
+
+export const FONT_SMALL_BLACK = {
+  fontFamily: FONT_FAMILY,
+  fontWeight: FONT_WEIGHT_MEDIUM,
+  fontSize: FONT_SIZE_12,
+  color: Colors.BLACK
+};
+
+export const FONT_SMALL_PINK = {
+  fontFamily: FONT_FAMILY,
+  fontWeight: FONT_WEIGHT_MEDIUM,
+  fontSize: FONT_SIZE_12,
+  color: Colors.PINK
+};
+
+export const FONT_TAG = {
+  fontFamily: FONT_FAMILY,
+  fontWeight: FONT_WEIGHT_MEDIUM,
+  fontSize: FONT_SIZE_10,
+  color: Colors.WHITE,
+  textAlign: "center"
+};
