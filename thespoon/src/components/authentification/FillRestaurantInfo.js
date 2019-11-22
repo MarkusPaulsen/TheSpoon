4a2846b449f3bef62527582e2122d2254565cdbc
zulicek <lora.zulicek@fer.hr>
2019-11-22 12:14:42 +0100

    Enabled adding opening hours for the restaurant

--- thespoon/src/components/authentification/FillRestaurantInfo.js
+++ thespoon/src/components/authentification/FillRestaurantInfo.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import {IconName, IconLocation, IconBirthday, IconExit, IconBack} from '../Icons';
 import {Modal} from "react-bootstrap";
 import FilterLink from "../../containers/FilterModalLink";
@@ -6,15 +7,107 @@ import {modalVisibilityFilters} from "../../constants/modalVisibiltyFilters";
 import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
 import Button from 'react-validation/build/button';
+import Select from 'react-select'
 
 
 class FillRestaurantInfo extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleChangeDay = this.handleChangeDay.bind(this);
+        this.handleChangeStartHour = this.handleChangeStartHour.bind(this);
+        this.handleChangeEndHour = this.handleChangeEndHour.bind(this);
+        this.addHours = this.addHours.bind(this);
+        this.removeHours = this.removeHours.bind(this);
+        
+        this.state = {
+            selectedOpeningHours: [],
+            selectedDay: null,
+            selectedStartHour: null,
+            selectedEndHour: null
+        }
+    }
+
+    handleChangeDay = (selectedDay) => {
+      this.setState({ selectedDay });
+    }
+
+    handleChangeStartHour = (selectedStartHour) => {
+      this.setState({ selectedStartHour });
+    }
+
+     handleChangeEndHour = (selectedEndHour) => {
+      this.setState({ selectedEndHour });
+    }
+
+    addHours = () => {
+        const newOpeningHours =  {
+            day: this.state.selectedDay,
+            startHour: this.state.selectedStartHour,
+            endHour: this.state.selectedEndHour
+        }
+       
+       this.setState({ selectedOpeningHours: [...this.state.selectedOpeningHours, newOpeningHours] });
+    }
+
+    removeHours = (openingHour) => {
+       this.setState({selectedOpeningHours: this.state.selectedOpeningHours.filter(function(oH) { 
+        return oH !== openingHour
+        })});
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+
     render() {
+        const days = [
+          { value: 'monday', label: 'Monday' },
+          { value: 'tuesday', label: 'Tuesday' },
+          { value: 'wednesday', label: 'Wednesday' },
+          { value: 'thursday', label: 'Thursday' },
+          { value: 'friday', label: 'Friday' },
+          { value: 'saturday', label: 'Saturday' },
+          { value: 'sunday', label: 'Sunday' }
+        ];
+
+        const hours = [
+            { value: '00', label: '00' },
+          { value: '01', label: '01' },
+          { value: '02', label: '02' },
+          { value: '03', label: '03' },
+          { value: '04', label: '05' },
+          { value: '06', label: '07' },
+          { value: '08', label: '09' },
+          { value: '10', label: '10' },
+          { value: '11', label: '11' },
+          { value: '12', label: '12' },
+          { value: '13', label: '13' },
+          { value: '14', label: '15' },
+          { value: '16', label: '17' },
+          { value: '18', label: '18' },
+          { value: '19', label: '19' },
+          { value: '20', label: '20' },
+          { value: '21', label: '21' },
+          { value: '22', label: '22' },
+          { value: '23', label: '23' }
+        ];
+
+        const selectStyles = {
+          menu: () => ({
+            fontSize: 15,
+            fontWeight: 500,
+            textAlign: "left",
+            backgroundColor: "#ffffff"
+          })
+        }
         return (
             <Modal.Body>
                 <span className="back"> <FilterLink filter={modalVisibilityFilters.SHOW_REGISTER_RESTAURANT_OWNER}><IconBack /></FilterLink></span>
                 <button className="exit" onClick={this.props.onHide}><IconExit /></button>
-                <div className="modal-wrapper ">
+                <div className="modal-wrapper restaurant-info">
                     <Form ref={ (c) => { this.form = c; }} onSubmit={(e) => this.handleSubmit(e)}>
                     <h2>Sign up</h2>
                     <div className="account-type">
@@ -23,29 +116,61 @@ class FillRestaurantInfo extends Component {
 
                     <div className="input-field">
                         <IconName />
+                        <label>Name</label>
                         <Input type="restaurantName" name="restaurantName" placeholder="Restaurant name"/>
                     </div>
 
                     <div className="input-field">
                         <IconLocation />
+                        <label>Address</label>
                         <Input type="address" name="address" placeholder="Address"/>
-                    </div>
-
-                    <div className="input-field name">
-                        <IconLocation />
-                        <Input type="city" name="city" placeholder="City"/>
-                        <Input type="country" name="country" placeholder="Country"/>
-                    </div>
-
-                    <div className="input-field name">
-                        <IconBirthday />
-                        <Input type="time" name="opening-hours"/>
-                        <Input type="time" name="opening-hours"/>
                     </div>
 
                     <div className="input-field">
                         <IconLocation />
-                        <Input type="tags" name="tags" placeholder="Tags"/>
+                        <label>City</label>
+                        <Input type="city" name="city" placeholder="City"/>
+                    </div>
+
+                    <div className="input-field">
+                        <IconLocation />
+                        <label>Country</label>
+                        <Input type="country" name="country" placeholder="Country"/>
+                    </div>
+
+                    <div className="input-field opening-hours">
+                            <IconBirthday />
+                            <label>Openening hours</label>
+                        <div className="hours-selector">
+                            <Select options={days} 
+                                onChange={this.handleChangeDay} 
+                                placeholder="Day" 
+                                styles={selectStyles} 
+                                className="days"/>
+                            <Select options={hours} 
+                                onChange={this.handleChangeStartHour} 
+                                placeholder="Start" 
+                                styles={selectStyles} 
+                                className="hours start"/>
+                            <Select options={hours} 
+                                onChange={this.handleChangeEndHour} 
+                                placeholder="End" 
+                                styles={selectStyles}  
+                                className="hours end"/>
+                        </div>
+                        <div className="add-button"><Button onClick={this.addHours}>+ Add</Button></div>
+                        <div className="selected-hours">
+                            {this.state.selectedOpeningHours.map((oh,i) => 
+                                <div key={i}>
+                                    {oh.day.label}: {oh.startHour.label} - {oh.endHour.label}
+                                    <span onClick={() => this.removeHours(oh)} 
+                                        role="button"
+                                        className="remove-button">
+                                        X
+                                    </span>
+                                </div>) 
+                            }
+                        </div>
                     </div>
 
                     <Button type="submit" className="normal">Sign up</Button>
