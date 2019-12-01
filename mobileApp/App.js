ffae18890a2456e3d28d17825455c37f8fb4540c
Cathrine Akre-Aas <cathrine-97@hotmail.com>
2019-12-01 18:50:40 +0100

    Add token to AsyncStorage

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { AsyncStorage, StyleSheet, Text, View, Image } from "react-native";
 import LoginScreen from "./components/login/login";
 import SearchPage from "./components/search/search";
 import LandingPage from "./components/landingpage/landingpage";
@@ -11,14 +11,14 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import * as Font from "expo-font";
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon from "react-native-vector-icons/MaterialIcons";
 import * as Colors from "./styles/colors";
 
 const SearchStack = createStackNavigator(
   {
     Search: SearchPage,
     Loading: LoadingPage,
-    Menu: MenuPage,
+    Menu: MenuPage
   },
   {
     initialRouteName: "Search",
@@ -28,64 +28,63 @@ const SearchStack = createStackNavigator(
 );
 
 const ReviewStack = createStackNavigator(
-    {
-      Review: ReviewPage,
-    }  ,
-    {
-      initialRouteName: "Review",
-      header: null,
-      headerMode: "none"
-    }
+  {
+    Review: ReviewPage,
+    Login: LoginScreen
+  },
+  {
+    initialRouteName: "Login",
+    header: null,
+    headerMode: "none"
+  }
 );
-
 const ProfileStack = createStackNavigator(
-    {
-      Login: LoginScreen,
-      Profile: ProfilePage
-    }  ,
-    {
-      initialRouteName: "Profile",
-      header: null,
-      headerMode: "none"
-    }
+  {
+    Login: LoginScreen,
+    Profile: ProfilePage
+  },
+  {
+    initialRouteName: "Login",
+    header: null,
+    headerMode: "none"
+  }
 );
 
 const bottomTabNavigator = createBottomTabNavigator(
-    {
-      Search: {
-        screen: SearchStack,
-        navigationOptions: {
-          tabBarIcon: ({ tintColor }) => (
-              <Icon name="search" size={32} color={tintColor}/>
-          )
-        }
-      },
-      AddReview: {
-        screen: ReviewStack,
-        navigationOptions: {
-          tabBarIcon: ({ tintColor }) => (
-              <Icon name="add-circle-outline" size={32} color={tintColor}/>
-          )
-        }
-      },
-      Profile: {
-        screen: ProfileStack,
-        navigationOptions: {
-          tabBarIcon: ({ tintColor }) => (
-              <Icon name="person" size={32} color={tintColor}/>
-          )
-        }
-      },
+  {
+    Search: {
+      screen: SearchStack,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="search" size={32} color={tintColor} />
+        )
+      }
     },
-    {
-      initialRouteName: "Search",
-      tabBarOptions: {
-        activeTintColor: "#F3A3A3",
-        height: 85
+    AddReview: {
+      screen: ReviewStack,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="add-circle-outline" size={32} color={tintColor} />
+        )
+      }
+    },
+    Profile: {
+      screen: ProfileStack,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="person" size={32} color={tintColor} />
+        )
       }
     }
+  },
+  {
+    initialRouteName: "Search",
+    tabBarOptions: {
+      activeTintColor: "#F3A3A3",
+      height: 85
+    }
+  }
 );
-
 
 const AppContainer = createAppContainer(bottomTabNavigator);
 
@@ -100,14 +99,13 @@ const styles = StyleSheet.create({
 
 export default class App extends Component {
   state = {
-    fontLoaded: false
+    fontLoaded: false,
   };
 
   async componentDidMount() {
     await Font.loadAsync({
       roboto: require("./assets/fonts/roboto-regular.ttf")
     });
-
     this.setState({ fontLoaded: true });
   }
   render() {
--- mobileApp/components/profile/profile.js
+++ mobileApp/components/profile/profile.js
@@ -5,16 +5,22 @@ import {
   Text,
   TouchableOpacity,
   Image,
-  StyleSheet
+  StyleSheet, AsyncStorage
 } from "react-native";
 import * as Typography from "../../styles/typography";
 import * as Colors from "../../styles/colors";
 
 export default class Review extends Component {
+  constructor(props){
+    super(props);
+  }
+
   render() {
+    const {navigation} = this.props;
+    const username = navigation.getParam("username", "000");
     return (
       <ScrollView contentContainerStyle={styles.container}>
-        <Text>Profile page</Text>
+        <Text>Welcome to your profile {username}</Text>
       </ScrollView>
     );
   }
