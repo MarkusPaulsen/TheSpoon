390ea91b65e386e330d6825fde79a74513a51e93
Frikkha <frikk.hald.andersen@gmail.com>
2019-12-10 11:38:43 +0100

    Added files for sprint6

--- router.js
+++ router.js
@@ -13,22 +13,26 @@ const image = require('./routes/image-upload.js');
 const searchByMenuItem = require('./routes/searchByMenuItem.js');
 const manageMenuInformation = require('./routes/manageMenuInformation.js');
 const searchMenu = require('./routes/searchMenu.js');
-const getRestaurant = require('./routes/manageRestaurantInformation.js');
+const manageRestaurantInformation = require('./routes/manageRestaurantInformation.js');
 const manageCustomerReviews = require('./routes/manageCustomerReviews.js');
 const manageCustomerInformation = require('./routes/manageCustomerInformation.js');
 const createReview = require('./routes/createReview.js');
+const getTags = require('./routes/getTags.js');
+const pendingReviews = require('./routes/pendingReviews.js');
 
 app.use('/api/user/login', login);
 app.use('/api/user/customer/register', registrationCustomer);
 app.use('/api/user/owner/register', registrationOwner);
-app.use('/api/image',image);
+app.use('/api/image', image);
 app.use('/api/user/customer/menu/searchByMenuItem', searchByMenuItem);
 app.use('/api/user/owner/restaurant/menu', manageMenuInformation);
 app.use('/api/user/customer/menu/', searchMenu);
-app.use('/api/user/owner/restaurant', getRestaurant);
+app.use('/api/user/owner/restaurant', manageRestaurantInformation);
 app.use('/api/user/customer/review/restaurant', createReview);
 app.use('/api/user/customer/review', manageCustomerReviews);
 app.use('/api/user/customer', manageCustomerInformation);
+app.use('/api/user/owner/tag', getTags);
+app.use('/api/user/owner/restaurant/review', pendingReviews);
 
 app.use(bodyParser.json());
 
---
+++ routes/getTags.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express();
+router.use(express.json());
+
+module.exports = router;
