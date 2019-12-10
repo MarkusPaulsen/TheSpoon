844932ee4efef189b8cefe3f177712ad1dd9a601
janinestang <helenejs@stud.ntnu.no>
2019-12-10 08:59:13 +0100

    Added functionality and layout for see review

--- mobileApp/components/profile/profile.js
+++ mobileApp/components/profile/profile.js
@@ -8,11 +8,15 @@ import {
   StyleSheet,
   AsyncStorage,
   FlatList,
-  SafeAreaView
+  SafeAreaView,
+  ScrollView,
+  Modal,
+  Dimensions
 } from "react-native";
 import * as Typography from "../../styles/typography";
 import * as Colors from "../../styles/colors";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import { AirbnbRating } from "react-native-ratings";
 
 export default class Profile extends Component {
   constructor(props) {
@@ -20,25 +24,68 @@ export default class Profile extends Component {
     this.state = {
       loggedIn: false,
       isLoaded: false,
+      modalItem: null,
       userInfo: { username: "cathrbak", email: "cathrineakreaaas@gmail.com" },
       reviews: [
         {
           reviewID: "2",
           menu: "Lunch menu",
           restaurant: "AUUM",
-          status: "approved"
+          status: "approved",
+          serviceRating: 4,
+          qualityOverPriceRating: 5,
+          menuItemsReviews: [
+            {
+              itemName: "Pizza Margherita",
+              score: 4,
+              text: "This is the review text"
+            },
+            {
+              itemName: "Blue Cheese Burger",
+              score: 3,
+              text: "This is the other review text"
+            }
+          ]
         },
         {
           reviewID: "3",
           menu: "Dinner menu",
           restaurant: "AUUM",
-          status: "pending"
+          status: "pending",
+          serviceRating: 4,
+          qualityOverPriceRating: 5,
+          menuItemsReviews: [
+            {
+              itemName: "Pizza Margherita",
+              score: 4,
+              text: "This is the review text"
+            },
+            {
+              itemName: "Blue Cheese Burger",
+              score: 3,
+              text: "This is the other review text"
+            }
+          ]
         },
         {
           reviewID: "9",
           menu: "Lunch menu",
           restaurant: "Da Zero",
-          status: "declined"
+          status: "declined",
+          serviceRating: 4,
+          qualityOverPriceRating: 5,
+          menuItemsReviews: [
+            {
+              itemName: "Pizza Margherita",
+              score: 4,
+              text: "This is the review text"
+            },
+            {
+              itemName: "Blue Cheese Burger",
+              score: 3,
+              text: "This is the other review text"
+            }
+          ]
         }
       ]
     };
@@ -57,32 +104,36 @@ export default class Profile extends Component {
     this.focusListener.remove();
   }
 
-
   logout() {
     Alert.alert(
-        'Logout',
-        'Are you sure you want to log out?',
-        [
-          {
-            text: 'Cancel',
-            onPress: () => console.log('Cancel logout'),
-            style: 'cancel',
-          },
-          {text: 'Logout', onPress: () => {console.log('Logout Pressed') ;  AsyncStorage.removeItem("userToken")
-                .then(() => {
-                  this.setState({ loggedIn: false });
-                  Alert.alert("Logout Success!");
-                })
-                .catch(error => console.log("Error logging out: ", error));}},
-        ],
-        {cancelable: false},
+      "Logout",
+      "Are you sure you want to log out?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel logout"),
+          style: "cancel"
+        },
+        {
+          text: "Logout",
+          onPress: () => {
+            console.log("Logout Pressed");
+            AsyncStorage.removeItem("userToken")
+              .then(() => {
+                this.setState({ loggedIn: false });
+                Alert.alert("Logout Success!");
+              })
+              .catch(error => console.log("Error logging out: ", error));
+          }
+        }
+      ],
+      { cancelable: false }
     );
-
   }
 
   async getUserInfo() {
     try {
-      const backendStubLink = `http://192.168.1.110:8080/api/user/customer/`;
+      const backendStubLink = `http://192.168.1.103:8080/api/user/customer/`;
       const backendServerLink = `https://thespoon.herokuapp.com/api/user/customer/`;
       const response = await fetch(backendStubLink, {
         method: "GET",
@@ -101,7 +152,7 @@ export default class Profile extends Component {
 
   async getUserReviews() {
     try {
-      const backendStubLink = `http://192.168.1.110:8080/api/user/customer/review`;
+      const backendStubLink = `http://192.168.1.103:8080/api/user/customer/review`;
       const backendServerLink = `https://thespoon.herokuapp.com/api/user/customer/review`;
       const response = await fetch(backendStubLink, {
         method: "GET",
@@ -114,57 +165,207 @@ export default class Profile extends Component {
         restaurant: index.restaurant,
         status: index.status
       }));
-      this.setState({ reviews });
+      //this.setState({ reviews });
     } catch (error) {
       console.log("Error fetching reviews: ", error);
     }
   }
+
+  async deleteReview(reviewID) {
+    try {
+      const backendStubLink = `http://192.168.1.103:8080/api/user/customer/review/${reviewID}`;
+      // FILL IN CORRECT LINK
+      const backendServerLink = `https://thespoon.herokuapp.com/api/user/customer/review`;
+      const response = await fetch(backendStubLink, {
+        method: "DELETE",
+        accept: "application/json"
+      });
+      const responseJson = await response.json();
+      console.log(responseJson);
+    } catch (error) {
+      console.log("Error deleting review: ", error);
+    }
+  }
+
+  getRating = score => {
+    return (
+      <AirbnbRating
+        isDisabled={true}
+        defaultRating={score}
+        showRating={false}
+        size={25}
+        selectedColor={Colors.PINK}
+      />
+    );
+  };
+
   render() {
-    const YourReviewItem = ({ menu, restaurant, status }) => {
+    const screenWidth = Math.round(Dimensions.get("window").width);
+
+    const YourReviews = ({ menu, restaurant, status, item }) => {
       return (
         <View style={styles.reviewItem}>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          <TouchableOpacity
+            onPress={() => {
+              this.setState({ modalItem: item });
+            }}
           >
-            <View style={{ flexDirection: "column" }}>
-              <View style={{ flexDirection: "row" }}>
-                <Text
-                  style={[
-                    Typography.FONT_H4_BLACK,
-                    { marginTop: 12, marginLeft: 21, marginBottom: 0 }
-                  ]}
-                >
-                  {menu}
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <View style={{ flexDirection: "column" }}>
+                <View style={{ flexDirection: "row" }}>
+                  <Text
+                    style={[
+                      Typography.FONT_H4_BLACK,
+                      { marginTop: 12, marginLeft: 21, marginBottom: 0 }
+                    ]}
+                  >
+                    {menu}
+                  </Text>
+                </View>
+                <Text style={[Typography.FONT_SMALL_BLACK, { marginLeft: 21 }]}>
+                  {restaurant}
                 </Text>
               </View>
-              <Text style={[Typography.FONT_SMALL_BLACK, { marginLeft: 21 }]}>
-                {restaurant}
-              </Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "flex-end",
+                  marginRight: 12,
+                  marginTop: 12
+                }}
+              >
+                {status === "approved" ? (
+                  <Icon name={"done"} size={35} color={Colors.GREEN} />
+                ) : null}
+                {status === "declined" ? (
+                  <Icon name={"clear"} size={35} color={Colors.PINK} />
+                ) : null}
+                {status === "pending" ? (
+                  <Text style={[Typography.FONT_MED_GRAY, { marginTop: 12 }]}>
+                    Pending...
+                  </Text>
+                ) : null}
+              </View>
             </View>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "flex-end",
-                marginRight: 12,
-                marginTop: 12
-              }}
-            >
-              {status === "approved" ? (
-                <Icon name={"done"} size={35} color={Colors.GREEN} />
-              ) : null}
-              {status === "declined" ? (
-                <Icon name={"clear"} size={35} color={Colors.PINK} />
-              ) : null}
-              {status === "pending" ? (
-                <Text style={[Typography.FONT_MED_GRAY, { marginTop: 12 }]}>
-                  Pending...
-                </Text>
-              ) : null}
-            </View>
-          </View>
+          </TouchableOpacity>
         </View>
       );
     };
+
+    const FullReviewModal = ({ review, visible }) => {
+      if (review == null) {
+        return null;
+      }
+      return (
+        <Modal animationType="slide" transparent={false} visible={visible}>
+          <ScrollView>
+            <View
+              style={[
+                styles.modalContainer,
+                {
+                  width: screenWidth * 0.85,
+                  marginLeft: (screenWidth * 0.15) / 2
+                }
+              ]}
+            >
+              <TouchableOpacity
+                onPress={() => this.setState({ modalItem: null })}
+              >
+                <Icon name={"chevron-left"} size={40} style={styles.button} />
+              </TouchableOpacity>
+              <View style={{ flexDirection: "row" }}>
+                <Text style={Typography.FONT_H2_BLACK}>Your </Text>
+                <Text style={Typography.FONT_H2_PINK}>Review</Text>
+              </View>
+              <View style={{ flexDirection: "row" }}>
+                <Text style={Typography.FONT_MED_GRAY}>Status: </Text>
+                <Text style={Typography.FONT_MED_GRAY}>{review.status}</Text>
+              </View>
+              <Text
+                style={{
+                  width: 120,
+                  height: 120,
+                  textAlign: "center",
+                  alignSelf: "center",
+                  backgroundColor: Colors.GRAY_LIGHT,
+                  marginTop: 20
+                }}
+              >
+                DISPLAY IMAGE HERE
+              </Text>
+              <View style={[styles.field, { marginVertical: 30 }]}>
+                <Text style={Typography.FONT_H4_BLACK}>Restaurant </Text>
+                <Text style={Typography.FONT_H4_PINK}>{review.restaurant}</Text>
+              </View>
+              <View style={[styles.field, { marginBottom: 10 }]}>
+                <Text style={Typography.FONT_H4_BLACK}>Menu </Text>
+                <Text style={Typography.FONT_H4_PINK}>{review.menu}</Text>
+              </View>
+              <View style={styles.line} />
+              <Text style={[Typography.FONT_H4_BLACK, { marginTop: 10 }]}>
+                What did you eat/drink?
+              </Text>
+              {review.menuItemsReviews.map(item => {
+                return (
+                  <View>
+                    <View style={[styles.field, { marginVertical: 15 }]}>
+                      <Text style={Typography.FONT_MED_BLACK}>Item</Text>
+                      <Text style={Typography.FONT_MED_PINK}>
+                        {item.itemName}
+                      </Text>
+                    </View>
+                    {this.getRating(item.score)}
+                    <View
+                      style={[styles.textBox, { width: screenWidth * 0.85 }]}
+                    >
+                      <Text style={styles.text}>{item.text}</Text>
+                    </View>
+                    <View style={styles.line} />
+                  </View>
+                );
+              })}
+              <Text style={[Typography.FONT_H4_BLACK, {marginTop: 15}]}>What's you overall impression?</Text>
+              <View style={{alignItems: "center"}}>
+                <Text style={[Typography.FONT_H4_BLACK, {marginTop: 20, marginBottom: 10}]}>Service</Text>
+                {this.getRating(review.serviceRating)}
+                <Text style={[Typography.FONT_H4_BLACK, {marginTop: 20, marginBottom: 10}]}>Quality/price</Text>
+                {this.getRating(review.qualityOverPriceRating)}
+                <TouchableOpacity
+                  style={styles.deleteButton}
+                  onPress={() =>
+                    Alert.alert(
+                      "Alert",
+                      "Are you sure you want to delete this review?",
+                      [
+                        {
+                          text: "No",
+                          onPress: () => console.log("No pressed"),
+                          style: "cancel"
+                        },
+                        {
+                          text: "Yes",
+                          onPress: () => console.log("Yes Pressed")
+                        }
+                      ],
+                      { cancelable: false }
+                    )
+                  }
+                >
+                  <Text
+                    style={[Typography.FONT_H4_WHITE, { textAlign: "center" }]}
+                  >
+                    DELETE
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ScrollView>
+        </Modal>
+      );
+    };
+
     const RenderLogin = props => {
       return props.navigation.navigate("Login", { parent: "Profile" });
     };
@@ -174,23 +375,26 @@ export default class Profile extends Component {
     if (this.state.isLoaded) {
       if (this.state.loggedIn) {
         return (
-          <View style={styles.container}>
+          <View
+            style={[
+              styles.container,
+              {
+                width: screenWidth * 0.85,
+                marginLeft: (screenWidth * 0.15) / 2
+              }
+            ]}
+          >
             <View
               style={{
-                flex: 2,
                 flexDirection: "row",
-                justifyContent: "space-between",
-                marginRight: 10,
-                marginTop: 85
+                justifyContent: "space-between"
               }}
             >
-              <View style={{ flexDirection: "row" }}>
+              <View style={{ flexDirection: "row", marginBottom: 20 }}>
                 <Text style={Typography.FONT_H2_BLACK}>Your </Text>
                 <Text style={Typography.FONT_H2_PINK}>Profile</Text>
               </View>
-              <View
-                style={{ flexDirection: "row", justifyContent: "flex-end" }}
-              >
+              <View style={{ flexDirection: "row" }}>
                 <TouchableOpacity
                   onPress={() => {
                     this.logout();
@@ -209,33 +413,37 @@ export default class Profile extends Component {
               <Text style={[Typography.FONT_REGULAR_THIN, styles.userInfoText]}>
                 {this.state.userInfo.username}
               </Text>
-              <View style={styles.line}></View>
+              <View style={styles.line} />
               <Text style={Typography.FONT_H4_BLACK}>Email</Text>
               <Text style={[Typography.FONT_REGULAR_THIN, styles.userInfoText]}>
                 {this.state.userInfo.email}
               </Text>
-              <View style={styles.line}></View>
+              <View style={styles.line} />
             </View>
-            <View style={{ flex: 1, flexDirection: "row" }}>
+            <View style={{ flexDirection: "row", marginBottom: 10 }}>
               <Text style={Typography.FONT_H3_BLACK}>Your </Text>
               <Text style={Typography.FONT_H3_PINK}>Reviews</Text>
             </View>
-            <View style={{ flex: 7 }}>
+            <View style={{ flex: 6 }}>
               <SafeAreaView style={styles.reviewsList}>
                 <FlatList
                   data={this.state.reviews}
                   contentContainerStyle={{ flex: 1 }}
                   renderItem={({ item }) => (
-                    <YourReviewItem
-                      reviewID={item.reviewID}
+                    <YourReviews
                       menu={item.menu}
                       restaurant={item.restaurant}
                       status={item.status}
+                      item={item}
                     />
                   )}
                   keyExtractor={item => item.reviewID}
                 />
               </SafeAreaView>
+              <FullReviewModal
+                review={this.state.modalItem}
+                visible={this.state.modalItem != null}
+              />
             </View>
           </View>
         );
@@ -250,8 +458,7 @@ export default class Profile extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginLeft: 39,
-    marginRight: 39
+    marginTop: 60
   },
   logout_icon: {
     marginTop: 60
@@ -260,14 +467,13 @@ const styles = StyleSheet.create({
     marginTop: 10
   },
   line: {
-    marginTop: 15,
+    marginVertical: 5,
     borderBottomWidth: 1,
     borderBottomColor: Colors.GRAY_LIGHT
   },
 
   reviewsList: {
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: "center"
   },
   reviewItem: {
     backgroundColor: Colors.GRAY_LIGHT,
@@ -275,5 +481,35 @@ const styles = StyleSheet.create({
     height: 65,
     marginVertical: 8,
     borderRadius: 5
+  },
+  modalContainer: {
+    backgroundColor: Colors.WHITE,
+    marginBottom: 20
+  },
+  textBox: {
+    height: 150,
+    borderRadius: 5,
+    backgroundColor: Colors.GRAY_TEXTBOX,
+    marginVertical: 15,
+    alignSelf: "center"
+  },
+  text: {
+    width: 270,
+    height: 130,
+    marginTop: 10,
+    alignSelf: "center"
+  },
+  deleteButton: {
+    width: 150,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: Colors.GRAY_MEDIUM,
+    marginBottom: 30,
+    marginTop: 40,
+    justifyContent: "center"
+  },
+  field: {
+    flexDirection: "row",
+    justifyContent: "space-between"
   }
 });
