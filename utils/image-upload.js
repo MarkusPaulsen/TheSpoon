b515e80bb0d7e1d85d74dc3b89df094264fa94b2
Cathrine Akre-Aas <cathrbak@stud.ntnu.no>
2019-12-10 22:06:18 +0100

    Revert image-upload.js

--- utils/image-upload.js
+++ utils/image-upload.js
@@ -4,34 +4,34 @@ const multerS3 = require('multer-s3');
 const config = require('config');
 
 aws.config.update({
-  secretAccessKey: config.get('AWS_SECRET_ACCESS_KEY'),
-  accessKeyId: config.get('AWS_ACCESS_KEY_ID'),
-  region: 'eu-central-1'
+    secretAccessKey: config.get('AWS_SECRET_ACCESS_KEY'),
+    accessKeyId: config.get('AWS_ACCESS_KEY_ID'),
+    region: 'eu-central-1'
 });
 
 const s3 = new aws.S3();
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
-    cb(null, true);
-  } else {
-    cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
-  }
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
+    }
 };
 
 const upload = multer({
-  fileFilter,
-  storage: multerS3({
-    acl: 'public-read',
-    s3,
-    bucket: 'the-spoon',
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: 'TESTING_METADATA'});
-    },
-    key: function (req, file, cb) {
-      cb(null, Date.now().toString())
-    }
-  })
+    fileFilter,
+    storage: multerS3({
+        acl: 'public-read',
+        s3,
+        bucket: 'the-spoon',
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: 'TESTING_METADATA'});
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString())
+        }
+    })
 });
 
 module.exports = upload;
