2909b54be55c83014ac83a65c487ef03e0d9028b
marinco <marin.milina96@gmail.com>
2019-12-12 01:24:36 +0100

    set review status 'pending'

--- routes/createReview.js
+++ routes/createReview.js
@@ -11,6 +11,8 @@ const MenuReview = require('../models/menuReview.js');
 const MenuItem = require('../models/menuItem.js');
 const Restaurant = require('../models/restaurants.js');
 
+const PENDING='Pending';
+
 
 router.post('/menu/:menuID', auth, isCustomer, async (req, res) => {
     console.log('In POST /api/user/customer/review/restaurant/menu/' + req.params.menuID);
@@ -44,7 +46,7 @@ router.post('/menu/:menuID', auth, isCustomer, async (req, res) => {
         Date: req.body.date,
         ServiceRating: req.body.serviceRating,
         QualityRating: req.body.qualityOverPriceRating,
-        Status: "Pending",
+        Status: PENDING,
         Image_ID: req.body.receiptImageID
     });
 
