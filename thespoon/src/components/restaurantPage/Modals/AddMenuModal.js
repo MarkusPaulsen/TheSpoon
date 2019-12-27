19cec5f2806a6912653af4c1a7fdf09f2f1c8406
Markus Paulsen <ga96yen@mytum.de>
2019-12-27 09:03:59 +0100

    Implemented tag selection

--- thespoon/src/components/restaurantPage/Modals/AddMenuModal.js
+++ thespoon/src/components/restaurantPage/Modals/AddMenuModal.js
@@ -2,9 +2,9 @@
 import React, {Component} from "react";
 //</editor-fold>
 //<editor-fold desc="RxJs">
-import {bindCallback, of, throwError} from "rxjs";
+import {bindCallback, fromEvent, of, throwError} from "rxjs";
 import {ajax} from "rxjs/ajax";
-import {exhaustMap, map, take} from "rxjs/operators";
+import {catchError, exhaustMap, map, take, bufferTime, filter, distinctUntilChanged} from "rxjs/operators";
 //</editor-fold>
 //<editor-fold desc="Redux">
 import {connect} from "react-redux";
@@ -26,6 +26,7 @@ import {paths} from "../../../constants/paths";
 //<editor-fold desc="Icons">
 import {IconExit} from "../../Icons";
 import {timeout} from "../../../constants/timeout";
+import TagItem from "../Items/TagItem";
 
 //</editor-fold>
 
@@ -69,47 +70,138 @@ class AddMenuModal extends Component {
             },
             validWhen: true,
             message: "Description is required to be longer or equal 1 characters."
-        }, {
-            field: "tags",
-            method: "isEmpty",
-            validWhen: false,
-            message: "Tags are required."
-        }, /*{
-            field: "tags",
-            method: "isAlphanumeric (plus comma)",
-            validWhen: true,
-            message: "Tags are required to be alphanumeric."
-        },*/ {
-            field: "tags",
-            method: (tags) => {
-                return tags.split(",")
-                    .map((tag) => {
-                        return tag.trim()
-                    })
-                    .map((tag) => {
-                        return tag.length >= 1
-                    })
-                    .reduce((total, minLength) => {
-                        return total && minLength
-                    }, true)
-            },
-            validWhen: true,
-            message: "Each tag is required to be longer or equal 1 characters."
         }]);
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.update = this.update.bind(this);
 
         this.state = {
             name: "",
             description: "",
-            tags: "",
+            availableTags: [],
+            serverMessageFinishedLoadingAvailableTags:"",
+            finishedLoadingAvailableTags: false,
+            autocompleteTags: [],
+            chosenTags: [],
+            tagsMessage: "",
             validation: this.validator.valid(),
             serverMessage: "",
-            submitted: false
+            submitted: false,
+            toUpdate: false
         };
     }
 
     //</editor-fold>
+
+    componentDidMount() {
+        const thisTemp = this;
+
+        this.$availableTags = ajax({
+            url: paths["restApi"]["tag"],
+            method: "GET",
+            headers: {"X-Auth-Token": thisTemp.props.token},
+            timeout: timeout,
+            responseType: "text"
+        })
+            .pipe(
+                exhaustMap((next) => {
+                    let response = JSON.parse(next.response);
+                    return bindCallback(thisTemp.setState).call(thisTemp, {
+                        availableTags: response
+                    });
+                }),
+                catchError((error) => {
+                    throw error
+                }))
+            .subscribe(
+                () => {
+                    thisTemp.setState({serverMessageFinishedLoadingAvailableTags: "", finishedLoadingAvailableTags: true});
+                }, (error) => {
+                    switch (error.name) {
+                        case "AjaxTimeoutError":
+                            thisTemp.setState({serverMessageFinishedLoadingAvailableTags: ""  +"The request timed out.", finishedLoadingAvailableTags: true});
+                            break;
+                        case "InternalError":
+                        case "AjaxError":
+                            if (error.status === 0 && error.response === "") {
+                                thisTemp.setState({serverMessageFinishedLoadingAvailableTags: "There is no connection to the server.", finishedLoadingAvailableTags: true});
+                            } else if (error.status === 400) {
+                                this.props.openRestaurantConfiguration();
+                                thisTemp.setState({serverMessageFinishedLoadingAvailableTags: "", finishedLoadingAvailableTags: true});
+                            } else {
+                                thisTemp.setState({serverMessageFinishedLoadingAvailableTags: error.response, finishedLoadingAvailableTags: true});
+                            }
+                            break;
+                        default:
+                            console.log(error);
+                            thisTemp.setState({serverMessageFinishedLoadingAvailableTags: "Something is not like it is supposed to be.", finishedLoadingAvailableTags: true});
+                            break;
+                    }
+                }
+            );
+
+        this.$tags = fromEvent(document.getElementById("tagInput"), "input")
+            .pipe(map((event) => {
+                return event.target.value
+            }))
+            .pipe(bufferTime(1000))
+            .pipe(map((valueArray) => {
+                if(valueArray.length >= 1){
+                    return valueArray[valueArray.length-1]
+                }
+                else {
+                    return null
+                }
+            }))
+            .pipe(filter((value) => {
+                return value != null
+            }))
+            .pipe(distinctUntilChanged())
+            .pipe(map((value) => {
+                if(value.length >= 1) {
+                    return thisTemp.state.availableTags.filter((availableTag) => {
+                        return availableTag.startsWith(value)
+                    })
+                }
+                else {
+                    return []
+                }
+            }))
+            .subscribe(
+                (next) => {
+                    thisTemp.setState({
+                        autocompleteTags: next,
+                        tagsMessage: ""
+                    });
+                },
+                (error) => {
+                    console.log(error);
+                    thisTemp.setState({
+                        autocompleteTags: [],
+                        tagsMessage: "Something is not like it is supposed to be."
+                    });
+                }
+            );
+    }
+
+    componentWillUnmount() {
+        this.$availableTags.unsubscribe();
+        this.$tags.unsubscribe();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.state.toUpdate) {
+            this.setState({toUpdate: false});
+            this.componentWillUnmount()
+            this.componentDidMount()
+        }
+    }
+
+
+    update() {
+        this.setState({toUpdate: true});
+        this.forceUpdate()
+    }
 
     //<editor-fold desc="Bussiness Logic">
     handleSubmit = (event) => {
@@ -123,8 +215,7 @@ class AddMenuModal extends Component {
             .pipe(exhaustMap((values) => {
                 return bindCallback(thisTemp.setState).call(thisTemp, {
                     name: values.name,
-                    description: values.description,
-                    tags: values.tags.split(",").map(tag => tag.trim())
+                    description: values.description
                 });
             }))
             .pipe(exhaustMap(() => {
@@ -144,7 +235,7 @@ class AddMenuModal extends Component {
                         body: {
                             name: thisTemp.state.name,
                             description: thisTemp.state.description,
-                            tags: thisTemp.state.tags
+                            tags: thisTemp.state.chosenTags
                         },
                         timeout: timeout,
                         responseType: "text"
@@ -188,9 +279,7 @@ class AddMenuModal extends Component {
 
     //<editor-fold desc="Render">
     render() {
-        let validation = this.state.submitted ?                         // if the form has been submitted at least once
-            this.validator.validate(this.state) :               // then check validity every time we render
-            this.state.validation;
+        let validation = this.state.submitted ? this.validator.validate(this.state) : this.state.validation;
         return (
             <Modal.Body>
                 <button className="exit" onClick={this.props.onHide}><IconExit/></button>
@@ -217,11 +306,26 @@ class AddMenuModal extends Component {
                         </div>
 
                         <div className="input-field">
-                            <label>Tags</label>
-                            <Input type="text" name="tags" placeholder="Search"/>
+                            <label>Available Tags</label>
+                            <input id="tagInput" type="text" name="tags" placeholder="Search"/>
+                            <ul>
+                                {this.state.autocompleteTags.map((tag) => {
+                                    return (<TagItem tag={tag} modal={this} added={false}/>);
+                                })}
+                            </ul>
                         </div>
+
+                        <div className="input-field">
+                            <label>Chosen Tags</label>
+                            <ul>
+                                {this.state.chosenTags.map((tag) => {
+                                    return (<TagItem tag={tag} modal={this} added={true}/>);
+                                })}
+                            </ul>
+                        </div>
+
                         <div className="error-block">
-                            <small>{validation.tags.message}</small>
+                            <small>{this.state.tagsMessage}</small>
                         </div>
 
                         <Button type="submit" className="normal">Create</Button>
--- thespoon/src/constants/paths.js
+++ thespoon/src/constants/paths.js
@@ -1,5 +1,6 @@
 export const paths = {
     restApi: {
+        tag:                            "/api/user/owner/tag",
         registrationRestaurantOwner:    "/api/user/owner/register",
         restaurant:                     "/api/user/owner/restaurant",
         menu:                           "/api/user/owner/restaurant/menu",
