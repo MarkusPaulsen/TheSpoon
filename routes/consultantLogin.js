0df1c5af1e714c16ec306d56bc0b00622056872c
marinco <marin.milina96@gmail.com>
2019-12-28 15:18:57 +0100

    Add error handling

--- routes/consultantLogin.js
+++ routes/consultantLogin.js
@@ -13,25 +13,28 @@ const validationSchema = require('../validationSchemas.js');
 
 router.post('/', inputValidator(validationSchema.loginValidation), async (req, res) => {
     console.log('In POST /api/consultant/login');
+    try {
+        // check if there is a consultant with given username
+        const consultant = await Consultant.findOne({
+            where: {
+                Username: req.body.username
+            }
+        });
 
-    // check if there is a consultant with given username
-    const consultant = await Consultant.findOne({
-        where: {
-            Username: req.body.username
-        }
-    });
-
-    //if there isn't a consultant with given name, send error message
-    if (!consultant) res.status(400).send('Invalid username or password');
-    else {
-        //check if the password is correct
-        const isValid = await bcrypt.compare(req.body.password, consultant.Password);
-        if (!isValid) res.status(400).send('Invalid username or password');
+        //if there isn't a consultant with given name, send error message
+        if (!consultant) res.status(400).send('Invalid username or password');
         else {
-            //if the password is valid, send the token to the consultant
-            const token = jwt.sign({username: req.body.username}, config.get('jwtPrivateKey'), {expiresIn: config.get('jwtExpirationTime')});
-            res.status(201).send({token: token});
+            //check if the password is correct
+            const isValid = await bcrypt.compare(req.body.password, consultant.Password);
+            if (!isValid) res.status(400).send('Invalid username or password');
+            else {
+                //if the password is valid, send the token to the consultant
+                const token = jwt.sign({username: req.body.username}, config.get('jwtPrivateKey'), {expiresIn: config.get('jwtExpirationTime')});
+                res.status(201).send({token: token});
+            }
         }
+    }catch (error) {
+        res.status(500).send('Internal server error');
     }
 });
 
