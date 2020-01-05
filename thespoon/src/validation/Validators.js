92a273271ac203d7d25057be8fb46f8be250b0cb
Markus Paulsen <ga96yen@mytum.de>
2020-01-05 14:51:55 +0100

    Merge Stash Part 2

---
+++ thespoon/src/validation/Validators.js
@@ -0,0 +1,29 @@
+//<editor-fold desc="React">
+import React from "react";
+//</editor-fold>
+//<editor-fold desc="Validator">
+import validator from "validator";
+//</editor-fold>
+
+//<editor-fold desc="Business Logic">
+export const required = (value) => {
+    if (validator.isEmpty(value)) {
+        return (
+            <span className="error">
+                All fields must be fields out!
+            </span>
+        );
+    }
+};
+
+export const email = (value) => {
+    if (!validator.isEmail(value)) {
+        return (
+            <span className="error">
+               E-mail is incorrect!
+            </span>
+        );
+    }
+};
+//</editor-fold>
+
